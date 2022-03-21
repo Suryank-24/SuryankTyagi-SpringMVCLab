5aa532b4baed73bd2cb1033a358e1e60fa746fa8
Suryank Tyagi <88049742+Suryank-24@users.noreply.github.com>
2022-03-22 00:02:49 +0530

    first spring commit

---
+++ StudentManagement2/src/main/java/com/greatLearning/studentManagement/entity/Student.java
@@ -0,0 +1,79 @@
+package com.greatLearning.studentManagement.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="student")
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+
+
+	@Column(name="Name")
+	private String Name;
+
+
+	@Column(name="department")
+	private String department;
+	
+	@Column(name="country")
+	private String country;
+
+	public Student()
+	{
+		
+	}
+	
+	public Student(String Name, String department, String country) {
+		this.Name = Name;
+		this.department = department;
+		this.country = country;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String Name) {
+		this.Name = Name;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String Department) {
+		this.department = Department;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", Name=" + Name + "+  department=" + department + " + , country=" + country + "]";
+	}
+	
+}
